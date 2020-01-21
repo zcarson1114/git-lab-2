179d46c083856f4e2cfe76e1b1dfcef5213800f2
zcarson1114 <zc248518@ohio.edu>
2020-01-21 10:47:15 -0500

    Initial Commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Zachary Carson
+ *        Date: 1/21/20
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+    cout << "Hello Git!!" << argc;
+    /*add code*/
+    return 0;
+}// main
